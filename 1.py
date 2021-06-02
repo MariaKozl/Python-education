8c996f092c7c78214b7d0bc7743c025638a6d376
Maria <mashin@ro.ru>
2021-06-03 00:07:41 +0300

    add 1.pyadd 1.py

---
+++ 1.py
@@ -0,0 +1,25 @@
+# int - целые числа
+num_1 = 2
+num_2 = 55
+print(num_1, num_2)
+
+# float - дробные числа
+f_num_1 = 35.
+f_num_2 = -20.4
+print(f_num_1, f_num_2)
+
+# str - строка
+text = 'hello world'
+print(text)
+
+# bool - логическая переменная
+a = True
+b = False
+print(a, b)
+
+# запросить у пользователя и вывести на экран
+name = input('Введите ваше имя - ')
+print('Добрый день,', name)
+
+number = int(input('Введите целое число '))
+print('Вы ввели число -', number)
