1b4be9572b27e743c9ae5280b20251484f94edd7
Maria <mashin@ro.ru>
2021-06-07 21:09:25 +0300

    add 3.p

--- 3.py
+++
@@ -1,5 +0,0 @@
-number = int(input('Введите целое однозначное число '))
-number_2 = number * 10 + number
-number_3 = number * 100 + number_2
-summ = number + number_2 + number_3
-print(f'{number}+{number_2}+{number_3}={summ}')
